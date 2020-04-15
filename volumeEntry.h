15df2dce8664ba8511bc38ca029f41df288a6507
Alexander Beers <alexbeerz@yahoo.com>
2020-04-14 18:06:10 -0700

    Work from zoom call.

--- volumeEntry.h
+++ volumeEntry.h
@@ -1,31 +1,35 @@
 typedef struct VolumeEntry { 
     int numLba;
-    int freeInfoLocations[3]; //for '3' copies of free info locations in volume
+    int freeInfoLocation; //for '3' copies of free info locations in volume
     int rootdirLocation; //location of root directory
     char volumeName[128];
     unsigned int volumeID;
     unsigned int lbaSize;// = 512;
     unsigned int clusterSize;// = 8;
-} Ventry;
+} volumeEntry;
 
-struct freeChunk {
+typedef struct freeChunk {
     char signature1[8];
-
-    int next; //index of next chunk in LBA
-    int prev; //index of prev chunk in LBA
-    long count;
-
-    char sig2[8];
-};
-
-//opposite of free space
-struct extents {
-    char signature1[8];
-
-    int fileID; //can be directory as well
-    int next; //next chunk of file
-    int prev; //prev chunk of file
+    
+    unsigned int inUse;
+    unsigned int location;
+    //int next; //index of next chunk in LBA
+    //int prev; //index of prev chunk in LBA
     long count;
 
     char signature2[8];
-};
+} freeChunk;
+
+//opposite of free space
+typedef struct extent {
+    char signature1[8];
+
+    unsigned int inUse;
+    unsigned int location;
+    int fileID; //can be directory as well
+    //int next; //next chunk of file
+    //int prev; //prev chunk of file
+    long count;
+
+    char signature2[8];
+} usedChunk;
