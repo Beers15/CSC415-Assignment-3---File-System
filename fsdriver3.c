69f95990edfad7c1c930b96e7eb2c3f844a7f9f9
Alexander Beers <alexbeerz@yahoo.com>
2020-04-17 15:39:05 -0700

    Init finished.

--- fsdriver3.c
+++ fsdriver3.c
@@ -25,7 +25,7 @@
 #include "entry.h"
 #include "volumeEntry.h"
 
-int init(volumeEntry* vcb, char bitMap[], entry entries[], uint64_t volumeSize, uint64_t blockSize, uint64_t lbaCount, char* fileName);
+int init(volumeEntry* vcb, char bitMap[], char* bitMapBuf, entry entries[], entry* entryList, uint64_t volumeSize, uint64_t blockSize, uint64_t lbaCount, char* fileName);
 
 int main(int argc, char* argv[]) {
 	uint64_t vSize = 500000;
@@ -40,27 +40,33 @@ int main(int argc, char* argv[]) {
 
 	//metadata
 	volumeEntry* vcb = malloc(*blockSize);
-	char* bitMap = malloc(((sizeof(char) * lbaCount) / bSize) + 1);		
-	entry* entries = malloc(((sizeof(entry) * lbaCount) / bSize) + 1);
+	entry* entryList = malloc((((sizeof(entry) * lbaCount) / bSize) + 1) * bSize );
+	char bitMap[lbaCount];
+	entry entries[lbaCount];
+	char* bitMapBuf = malloc((((sizeof(char) * lbaCount) / bSize) + 1) * bSize);
+	int numMetadataBlocks = init(vcb, bitMap, bitMapBuf, entries, entryList, *volSize, *blockSize, lbaCount, fileName);
+	init(vcb, bitMap, bitMapBuf, entries, entryList, *volSize, *blockSize, lbaCount, fileName);
+	
+	printf("The number of blocks used for metadata is %d blocks out of %ld blocks.\n", numMetadataBlocks, lbaCount);
 
-	int numMetadataBlocks = init(vcb, bitMap, entries, *volSize, *blockSize, lbaCount, fileName);
-	printf("The number of blocks used for metadata is %d blocks out of %ld blocks.", numMetadataBlocks, lbaCount);
-
-
-	// //check if vcb values were obtained
-	printf("\nVCB Start of Volume: %c\n", vcb->startOfVolume);
+	// Testing to see if vcb works here
+	printf("VCB Start of Volume: %c\n", vcb->startOfVolume);
 	printf("VCB Num LBA:		 %d\n", vcb->numLba);
 	printf("VCB Volume Name:	 %s\n", vcb->volumeName);
 	printf("VCB Volume ID:		 %d\n", vcb->volumeID);
 	printf("VCB LBA Size:		 %d\n", vcb->lbaSize);
-
-	uint64_t statusForRead2 = LBAread(bitMap, 2, 1);
-	//out bitmap array values for testing here
-	for(int i = 0; i < lbaCount; i++) {
-	  	printf("Bitmap Index %d: %c\n", i, *(bitMap + i));
+	
+	
+	for (int i = 0; i < vcb->numLba; i++) {
+		printf("Main Bitmap Index %d: %c\n", i, *(bitMapBuf + i));
 	}
+	
+	
 
-
+	for (int i = 0; i < vcb->numLba; i++) {
+		printf("Main Entry Index %d: %s\n", i, (entryList + i)->name);
+	}
+	
 
 	//after init works main loop would go here prompting for commands
 	
@@ -70,17 +76,18 @@ int main(int argc, char* argv[]) {
 }
 
 //the params here will have to be assigned to whatever already exists on the volume if it isn't a new volume
-int init(volumeEntry* vcb, char bitMap[], entry entries[], uint64_t volumeSize, uint64_t blockSize, uint64_t lbaCount, char* fileName) {
+int init(volumeEntry* vcb, char bitMap[], char* bitMapBuf, entry entries[], entry* entryList, uint64_t volumeSize, uint64_t blockSize, uint64_t lbaCount, char* fileName) {
+	//int blocksNeeded = #; //calculate after figturing out init block size for all metadata
 	int trackPosition = 0; 
-    uint64_t statusForRead = LBAread(vcb, 1, 0);
+	uint64_t statusForRead = LBAread(vcb, 1, 0);
 
 	if(vcb->startOfVolume == '^') {
+		trackPosition += 1; //vcb is always < 1 full block
 		printf("existing volume, read in metadata to program\n");
-
-		//read in existing bitMap array
-		uint64_t statusForRead2 = LBAread(bitMap, (((sizeof(char) * lbaCount) / blockSize) + 1), trackPosition);
-	
-
+		uint64_t statusForRead2 = LBAread(bitMapBuf, (((sizeof(char) * lbaCount) / blockSize) + 1), trackPosition);
+		trackPosition += (((sizeof(char) * lbaCount) / blockSize) + 1);
+		uint64_t statusForRead3 = LBAread(entryList, (((sizeof(entry) * lbaCount) / blockSize) + 1), trackPosition);
+		trackPosition += (((sizeof(entry) * lbaCount) / blockSize) + 1);
 	} else {
 		printf("this is a new volume INIT STUFF\n");	
 
@@ -97,47 +104,31 @@ int init(volumeEntry* vcb, char bitMap[], entry entries[], uint64_t volumeSize, 
 		vcb = &vcbTemp;
 		LBAwrite(vcb, 1, trackPosition);
 		trackPosition += 1; //vcb is always < 1 full block
-
-		//this test confirms vcb was written properly
-		// volumeEntry* testbuffer = malloc(blockSize);
-		// int blocksread = LBAread(testbuffer, 1, 0);
-		// printf("%c\n", testbuffer->startOfVolume);
-		// printf("%d\n", testbuffer->numLba);
-		// printf("%s\n", testbuffer->volumeName);
-		// printf("%d\n", testbuffer->volumeID);
-		// printf("%d\n", testbuffer->lbaSize);
-
+		
 
 		//write bitMap array to volume
 		for(int i = 0; i < lbaCount; i++) {
-			if(i < (trackPosition + (((sizeof(char) * lbaCount) / blockSize) + 1)))
+			if(i < (trackPosition + (((sizeof(entry) * lbaCount) / blockSize) + 1))) {
 				bitMap[i] = '1'; //all the blocks used for metadata are in use
-			else 
+			}
+			else {
 				bitMap[i] = '0'; //remainder are free
+			}
 		}
-
-		char* bitMapBuf = bitMap;
+		
+		bitMapBuf = bitMap;
 		int bitMapBlksWritten = LBAwrite(bitMapBuf, (((sizeof(char) * lbaCount) / blockSize) + 1), trackPosition);
-		trackPosition+= bitMapBlksWritten;
-		//printf("Bit map blocks written %d\n, trackPosition: %d\n", bitMapBlksWritten, trackPosition);
-		//printf("size of empty bitMap array is %ld blocks\n", (((sizeof(char) * lbaCount) / blockSize) + 1));
-
-				
-
+		trackPosition += (((sizeof(char) * lbaCount) / blockSize) + 1);
 		
-		//printf("size of empty entry array is %ld blocks\n", (((sizeof(entry) * lbaCount) / blockSize) + 1));
+		for (int i = 0; i < lbaCount; i++) {
+			strcpy(entries[i].name, "-1");
+		}
 		
-		// //write an empty entry array of the max possible entry size to the new volume
-		// entry* entryListBuf = entries;
-		// int entryListBlksWritten = LBAwrite(entryListBuf, (((sizeof(entry) * lbaCount) / blockSize) + 1) , trackPosition);
-		// trackPosition += entryListBlksWritten;
-
-
-
-
-		//TODO test if the volume contains the write values for bitmap and entries array
+		//write an empty entry array of the max possible entry size to the new volume
+		entryList = entries;
+		printf("%s\n", (entryList + 3)->name);
+		int entryListBlksWritten = LBAwrite(entryList, (((sizeof(entry) * lbaCount) / blockSize) + 1) , trackPosition);
+		//trackPosition += (((sizeof(entry) * lbaCount) / blockSize) + 1);
 	}
-	
-	return trackPosition;
-	
+	return trackPosition;	
 }
