75056add20374267595a0c47bd23a74070d41678
Mjcanson <mcanson@mail.sfsu.edu>
2020-04-17 20:50:34 -0700

    created command line prompt. NOTE for the two 'special commands', I just created 'cpn' for copying a directory from normal dir to current dur and 'cpc' for copying current dir to normal dir

--- fsdriver3.c
+++ fsdriver3.c
@@ -27,6 +27,20 @@
 
 int init(volumeEntry* vcb, char bitMap[], char* bitMapBuf, entry entries[], entry* entryList, uint64_t volumeSize, uint64_t blockSize, uint64_t lbaCount, char* fileName);
 
+//command prompt functions
+void listDir(char* args[]);
+void changeDir(char* args[]);
+void makeDir(char* args[]);
+void rmDir(char* args[]);
+void rmFile(char* args[]);
+void addFile(char* args[]);
+void cpFile(char* args[]);
+void mvFile(char* args[]);
+void copyNormaltoCurrent(char* args[]);
+void copyCurrenttoNormal(char* args[]);
+
+
+
 int main(int argc, char* argv[]) {
 	uint64_t vSize = 500000;
 	uint64_t* volSize = &vSize;
@@ -34,6 +48,82 @@ int main(int argc, char* argv[]) {
 	uint64_t bSize = 512;
 	uint64_t* blockSize = &bSize;
 
+	char line[1024];
+	char* args[500];
+
+  	while (1) {
+        if(argv[1] == NULL)
+            argv[1] = ">";
+        printf("%s ",argv[1]);
+
+        // reading user line
+     if(fgets(line, sizeof(line), stdin) == NULL){
+         exit(EXIT_SUCCESS);
+     }
+        line[strlen(line) - 1] = '\0';  // removes '\n' from fgets
+		printf("line: %s\n",line); //TEST
+
+		//error message for empty line
+        if(!strcmp(line,"\n")){
+            printf("Error: Empty line. Try again. \n");
+            continue;
+        }
+
+        //when user decides to exit
+        if (!strcmp(line, "exit")) {
+           return 0;
+        }
+
+        int i = 0;
+        args[0] = strtok(line, " ");
+		
+        while (args[i] != NULL) {
+            args[++i] = strtok(NULL, " ");
+			// printf("args: %s\n",args[i]); //TEST
+        }
+
+			printf("args[0]: %s\n",args[0]); //TEST
+			printf("args[1]: %s\n",args[1]); //TEST
+			printf("args[2]: %s\n",args[2]); //TEST
+
+
+			if(strcmp(args[0],"ls") == 0) {
+				listDir(args); 
+			}
+			else if(strcmp(args[0],"cd") == 0) {
+				changeDir(args);
+			}
+			else if(strcmp(args[0],"mkdir") == 0) {
+				makeDir(args);
+			}
+			else if(strcmp(args[0],"rmdir") == 0) {
+				rmDir(args);
+			}
+				else if(strcmp(args[0],"rm") == 0) {
+				rmFile(args);
+			}
+				else if(strcmp(args[0],"cat") == 0) {
+				addFile(args);
+			}
+			else if(strcmp(args[0],"cp") == 0) {
+				cpFile(args);
+			}
+			else if(strcmp(args[0],"mv") == 0) {
+				mvFile(args);
+			}
+			else if(strcmp(args[0],"cpn") == 0) {
+				copyNormaltoCurrent(args);
+			}
+			else if(strcmp(args[0],"cpc") == 0) {
+				copyCurrenttoNormal(args);
+			}
+			else{
+				//TODO not sure if theres some type of error message to call
+				printf("command not found.\n");
+			}
+
+
+	  }
 
 	int status = startPartitionSystem(fileName, volSize, blockSize);
 	uint64_t lbaCount = (*volSize / *blockSize) - 1;
@@ -47,7 +137,7 @@ int main(int argc, char* argv[]) {
 	int numMetadataBlocks = init(vcb, bitMap, bitMapBuf, entries, entryList, *volSize, *blockSize, lbaCount, fileName);
 	init(vcb, bitMap, bitMapBuf, entries, entryList, *volSize, *blockSize, lbaCount, fileName);
 	
-	printf("The number of blocks used for metadata is %d blocks out of %ld blocks.\n", numMetadataBlocks, lbaCount);
+	printf("The number of blocks used for metadata is %d blocks out of %llu blocks.\n", numMetadataBlocks, lbaCount);
 
 	// Testing to see if vcb works here
 	printf("VCB Start of Volume: %c\n", vcb->startOfVolume);
@@ -131,4 +221,44 @@ int init(volumeEntry* vcb, char bitMap[], char* bitMapBuf, entry entries[], entr
 		//trackPosition += (((sizeof(entry) * lbaCount) / blockSize) + 1);
 	}
 	return trackPosition;	
+}
+void listDir(char* args[]) {
+	//TODO
+	printf("listDir test: %s\n",args[0]); //TEST
+	
+}
+
+//Command line prompt functions
+
+void changeDir(char* args[]) {
+		//TODO
+}
+
+void makeDir(char* args[]) {
+		//TODO
+}
+
+void rmDir(char* args[]){
+		//TODO
+}
+
+void rmFile(char* args[]){
+		//TODO
+}
+
+void addFile(char* args[]){
+		//TODO
+}
+void cpFile(char* args[]){
+		//TODO
+}
+void mvFile(char* args[]){
+		//TODO
+}
+void copyNormaltoCurrent(char* args[]){
+		//TODO
+}
+
+void copyCurrenttoNormal(char* args[]){
+		//TODO
 }
